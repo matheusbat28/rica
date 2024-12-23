3f6235ca027eb32116a23cc912b4cb945da584e7
matheusbat28 <matheusarrow28@gmail.com>
2024-12-23 15:22:28 -0300

    adiciona configuração do Docker para frontend e Nginx, incluindo Dockerfile e nginx.conf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 3000,
+    host: true,
+    watch: {
+      usePolling: true,
+    },
+  },
 })
