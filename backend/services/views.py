a398fdc70a9c27343a88d012a9705c5bb4cc23a6
matheusbat28 <matheusarrow28@gmail.com>
2025-02-11 15:21:16 -0300

    adiciona controle de permissões ao ServiceViewSet para permitir acesso público na listagem

--- backend/services/views.py
+++ backend/services/views.py
@@ -1,7 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .serializers import ServiceSerializer
 from .models import Service
 
 class ServiceViewSet(viewsets.ModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
+
+    def get_permissions(self):
+        if self.action == 'list':
+            self.permission_classes = [permissions.AllowAny]
+        else:
+            self.permission_classes = [permissions.IsAuthenticated]
+        return super().get_permissions()
