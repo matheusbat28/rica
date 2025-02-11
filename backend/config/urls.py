bfd6405b51266dde15771af671e1bea0009b814e
matheusbat28 <matheusarrow28@gmail.com>
2025-02-11 13:57:17 -0300

    atualiza rotas da API para usar caminhos vazios em vez de 'api/'

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,8 +21,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('djoser.urls')),
-    path('api/', include('djoser.urls.jwt')),
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.jwt')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
